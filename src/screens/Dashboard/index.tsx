b4808021fe0ebfdcf51c35a6bd014fca373d2575
luishenriqueramalho <luishenriqueramalho.ramalhodasilva@gmail.com>
2024-05-14 13:03:34 -0300

    websocket

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { BinanceSocket } from "@/config/websocketConfig";
+import React, { useEffect } from "react";
 import { Text } from "react-native";
 
 const Dashboard: React.FC = () => {
+  useEffect(() => {
+    const symbol = "btcusdt";
+    const interval = "1m";
+    const binanceSocket = new BinanceSocket(symbol, interval);
+
+    return () => {
+      binanceSocket.close();
+    };
+  }, []);
+
   return (
     <>
       <Text>Dashboard</Text>
