6289a94b690d2a59d7ce0c6002a4a5170ecc933f
luishenriqueramalho <luishenriqueramalho.ramalhodasilva@gmail.com>
2024-05-16 13:18:26 -0300

    profile

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,21 +1,12 @@
 import NavigationBar from "@/components/NavigationBar";
 import { Wrapper } from "@/utils/global";
 import React from "react";
-import { Text } from "react-native";
-import { Header, PhotoProfile } from "./styles";
 
 const Profile: React.FC = () => {
   return (
     <>
       <NavigationBar title="Perfil" />
-      <Wrapper>
-        <Header>
-          <PhotoProfile>
-            <Text>Profile</Text>
-          </PhotoProfile>
-          <Text>Luis Henrique Ramalho</Text>
-        </Header>
-      </Wrapper>
+      <Wrapper></Wrapper>
     </>
   );
 };
