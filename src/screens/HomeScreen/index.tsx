9750b37d6198afec19eb131feac7c1f8e9628181
luishenriqueramalho <luishenriqueramalho.ramalhodasilva@gmail.com>
2024-05-16 13:44:14 -0300

    transition screens

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,14 +1,32 @@
 import BottomTab from "@/components/BottomTab";
 import NavigationBar from "@/components/NavigationBar";
 import { HomeWrapper, LinearBg, Wrapper } from "@/utils/global";
-import React, { useState } from "react";
-import { StyleSheet, Text } from "react-native";
+import React, { useRef, useState } from "react";
+import { Animated, StyleSheet, Text } from "react-native";
 import Dashboard from "../Dashboard";
 
 const HomeScreen: React.FC = () => {
   const [activeScreen, setActiveScreen] = useState<React.ReactNode>(
     <Dashboard />
   );
+  const opacity = useRef(new Animated.Value(1)).current;
+
+  const changeScreen = (screen: React.ReactNode) => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: true,
+    }).start(() => {
+      setActiveScreen(screen);
+
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 200,
+        useNativeDriver: true,
+      }).start();
+    });
+  };
+
   return (
     <LinearBg
       colors={["#614584", "#160F1E"]}
@@ -16,8 +34,12 @@ const HomeScreen: React.FC = () => {
       start={{ x: 4, y: 0 }}
       end={{ x: 1, y: 1 }}
     >
-      <HomeWrapper>{activeScreen}</HomeWrapper>
-      <BottomTab changeScreen={setActiveScreen} />
+      <HomeWrapper>
+        <Animated.View style={{ flex: 1, opacity }}>
+          {activeScreen}
+        </Animated.View>
+      </HomeWrapper>
+      <BottomTab changeScreen={changeScreen} />
     </LinearBg>
   );
 };
