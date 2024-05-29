ba926ae50c6b62510a0365f376a483ae32f0676d
luishenriqueramalho <luishenriqueramalho.ramalhodasilva@gmail.com>
2024-05-29 16:49:48 -0300

    price

--- src/screens/Home/Strategy.tsx
+++ src/screens/Home/Strategy.tsx
@@ -25,42 +25,61 @@ import { BinanceSocket } from "@/config/websocketConfig";
 import ChartsArea from "@/components/Charts/ChartsArea";
 import { Online } from "@/assets/svg/Online";
 
-const CardComponent: React.FC = ({ cryptoData }) => (
-  <CardSt>
-    <CardHeaderSt>
-      <ProfitSt>
-        <NameProfitSt>15% Profit</NameProfitSt>
-      </ProfitSt>
-      <View>
-        <Online />
-      </View>
-    </CardHeaderSt>
-    <CardGraphicSt>
-      <ChartsArea dataPrice={cryptoData} />
-    </CardGraphicSt>
-    <CardInfoSt>
-      <Active>
-        <ActiveBold>BNBERP32</ActiveBold>
-        <ActiveRegular>BNBERP32</ActiveRegular>
-      </Active>
-      <Prices>
-        <PricePrimary>$ 102k</PricePrimary>
-        <PriceSecundary>$ 495k / 1k investors</PriceSecundary>
-      </Prices>
-    </CardInfoSt>
-    <CardShareSt>
-      <You>
-        <Profile source={ProfilePicture} />
-        <TitleYou>Você</TitleYou>
-      </You>
-      <You>
-        <Profile source={ProfilePicture} />
-        <Profile source={ProfilePicture} />
-        <Profile source={ProfilePicture} />
-      </You>
-    </CardShareSt>
-  </CardSt>
-);
+const formatCurrency = (value) => {
+  return `$ ${parseFloat(value).toLocaleString("en-US", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })}`;
+};
+
+const CardComponent: React.FC<{ criptoData: any }> = ({ cryptoData }) => {
+  const price = cryptoData?.k?.c
+    ? formatCurrency(cryptoData.k.c)
+    : "Loading...";
+
+  console.log(">>>", cryptoData);
+
+  return (
+    <CardSt>
+      <CardHeaderSt>
+        <ProfitSt>
+          <NameProfitSt>15% Profit</NameProfitSt>
+        </ProfitSt>
+        <View>
+          <Online />
+        </View>
+      </CardHeaderSt>
+      <CardGraphicSt>
+        <ChartsArea dataPrice={cryptoData} />
+      </CardGraphicSt>
+      <CardInfoSt>
+        <Active>
+          <ActiveBold>
+            {cryptoData ? cryptoData?.k?.s : "Loading..."}
+          </ActiveBold>
+          <ActiveRegular>
+            {cryptoData ? cryptoData?.k?.s : "Loading..."}
+          </ActiveRegular>
+        </Active>
+        <Prices>
+          <PricePrimary>{price}</PricePrimary>
+          <PriceSecundary>$ 495k / 1k investors</PriceSecundary>
+        </Prices>
+      </CardInfoSt>
+      <CardShareSt>
+        <You>
+          <Profile source={ProfilePicture} />
+          <TitleYou>Você</TitleYou>
+        </You>
+        <You>
+          <Profile source={ProfilePicture} />
+          <Profile source={ProfilePicture} />
+          <Profile source={ProfilePicture} />
+        </You>
+      </CardShareSt>
+    </CardSt>
+  );
+};
 
 const Strategy: React.FC = () => {
   const [cryptoData, setCryptoData] = useState<any>(null);
